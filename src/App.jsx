8b491ee6749c194f1fecefa1d60e7109e38b2980
BubbaSantos <ultimatessambbo@gmail.com>
2026-06-30 23:53:46 +0100

    v2.14.11: increase header title offset to actually centre with emoji

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import { CSS } from '@dnd-kit/utilities'
 import products from './data/products.json'
 import './App.css'
 
-const VERSION = '2.14.10'
+const VERSION = '2.14.11'
 const SNAP = 80
 const AUTO = 220
 const QUEUE_KEY = 'trolley_queue'
